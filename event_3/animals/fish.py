d3835263754e81b211c39e7f6848080a06bfe92c
Alexandra Bogdanova <spikasw@yandex.ru>
2025-04-30 10:37:16 +0300

    event_3: animals were updated

--- event_3/animals/fish.py
+++ event_3/animals/fish.py
@@ -2,16 +2,21 @@ from animals.animal import Animal
 
 class Fish(Animal):
 
-    def __init__(self, file_name):
-        super().__init__(file_name)
+    def __init__(self, group_name):
+        super().__init__(group_name)
         # private member
-        self.__is_river_fish = self.data["is_river_fish"]
+        self.__is_river_fish = False
 
     # Imlements the abstract move() method of the parent Animal class.
     def move(self):
         print(f"{self.group_name} can swimming")
 
-    # Setter
+    # Getter
     @property
     def is_river_fish(self):
         return self.__is_river_fish
+    
+    # Setter
+    @is_river_fish.setter
+    def is_river_fish(self, value):
+        self.__is_river_fish = value
